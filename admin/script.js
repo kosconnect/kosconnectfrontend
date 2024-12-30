12cc6ffc453049b5901d056419839a7d66802b04
balqisrosa <balqisrosasekamayang@gmail.com>
2024-12-30 23:43:33 +0700

    dashboard admin

---
+++ admin/script.js
@@ -0,0 +1,5 @@
+function toggleDropdown() {
+    const dropdown = document.querySelector('.dropdown-menu');
+    // Toggle the display between none and block
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+}
